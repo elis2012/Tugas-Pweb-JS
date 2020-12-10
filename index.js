48746746d52dced0b1cf7a1a04ab684eb813083e
elisatri201299@gmail.com <elisatri201299@gmail.com>
2020-12-10 20:32:11 +0700

    modif

--- index.js
+++
@@ -1,1 +0,0 @@
-alert('Selamat Datang di Lakuin Yuks!')
--- sign up.js
+++ sign up.js
@@ -1,10 +1,12 @@
 function myFunction() {
     var x = document.getElementById("sign up");
-    var text = "";
+    var text ="";
     var i;
     for (i = 0; i < x.length ;i++) {
       text += x.elements[i].value + "<br>";
     }
     document.getElementById("simpan").innerHTML = text;
+    document.getElementById("simpan").innerHTML = email;
+    document.getElementById("simpan").innerHTML = password;
 }
 
