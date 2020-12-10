ae0c0035d425913cc677be7911f985df0b04a0f1
elisatri201299@gmail.com <elisatri201299@gmail.com>
2020-12-10 20:08:10 +0700

    tugasku

---
+++ sign up.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("sign up");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+      text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("simpan").innerHTML = text;
+}
+
